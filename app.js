54c43c55dec1822c50d6b97a11e94df76ed59430
Yash Lahoti <ylahoti2903@gmail.com>
2025-06-22 20:41:15 +0530

    commit

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ const validateListing= (req, res, next)=>{
   let {error}=listingSchema.validate(req.body);
   if(error){
     let errMsg= error.details.map((el)=> el.message).join(",")
-    throw new ExpressError(400, error)
+    throw new ExpressError(400, errMsg)
   } else{
     next();
   }
